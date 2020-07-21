38d26067f1dff7a556c258d246ba7eb1f225aacf
lucassds96 <lucassds96@outlook.com>
2020-07-21 15:12:37 -0300

    add detail activity

---
+++ app/src/main/java/com/example/myfreecomm/details/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myfreecomm.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfreecomm.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
