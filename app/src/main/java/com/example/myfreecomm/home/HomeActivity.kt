07e81a52fd2e22127e701a2cda4f226f8737fcee
lucassds96 <lucassds96@outlook.com>
2020-07-21 15:00:50 -0300

    add imports to project

---
+++ app/src/main/java/com/example/myfreecomm/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myfreecomm.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfreecomm.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
